6b28d31b02928c781c3133c4301a4634c790abbb
BandeDeTitoy <115489563+BandeDeTitoy@users.noreply.github.com>
2023-02-23 17:00:37 +0100

    Update test_s1q2_connected_components.py

--- tests/test_s1q2_connected_components.py
+++ tests/test_s1q2_connected_components.py
@@ -7,15 +7,25 @@ from graph import Graph, graph_from_file
 import unittest   # The test framework
 
 class Test_GraphCC(unittest.TestCase):
-    def test_network0(self):
+    def test_network00(self):
         g = graph_from_file("input/network.00.in")
         cc = g.connected_components_set()
         self.assertEqual(cc, {frozenset({1, 2, 3, 4, 5, 6, 7, 8, 9, 10})})
 
-    def test_network1(self):
+    def test_network01(self):
         g = graph_from_file("input/network.01.in")
         cc = g.connected_components_set()
         self.assertEqual(cc, {frozenset({1, 2, 3}), frozenset({4, 5, 6, 7})})
+        
+    def test_network02(self):
+        g = graph_from_file("input/network.02.in")
+        cc = g.connected_components_set()
+        self.assertEqual(cc,{frozenset({1,2,3,4}))
+                             
+    def test_network03(self):
+        g = graph_from_file("input/network.03.in")
+        cc = g.connected_components_set()
+        self.assertEqual(cc,{frozenset({1,2,3,4}))
 
 if __name__ == '__main__':
     unittest.main()
