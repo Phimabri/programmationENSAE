a6aa425caea80bcf35d600c8d03957aa320d009e
BandeDeTitoy <115489563+BandeDeTitoy@users.noreply.github.com>
2023-02-23 16:55:41 +0100

    Update test_s1q1_graph_loading.py

--- tests/test_s1q1_graph_loading.py
+++ tests/test_s1q1_graph_loading.py
@@ -6,21 +6,28 @@ import unittest
 from graph import Graph, graph_from_file
 
 class Test_GraphLoading(unittest.TestCase):
-    def test_network0(self):
+    def test_network00(self):
         g = graph_from_file("input/network.00.in")
         self.assertEqual(g.nb_nodes, 10)
         self.assertEqual(g.nb_edges, 9)
 
-    def test_network1(self):
+    def test_network01(self):
         g = graph_from_file("input/network.01.in")
         self.assertEqual(g.nb_nodes, 7)
         self.assertEqual(g.nb_edges, 5)
     
-    def test_network4(self):
+    def test_network04(self):
         g = graph_from_file("input/network.04.in")
         self.assertEqual(g.nb_nodes, 10)
         self.assertEqual(g.nb_edges, 4)
-        self.assertEqual(g.graph[1][0][2], 6)
-
+        self.assertIn([3,4,3],g.graph[2])
+        self.assertIn([4,4,2],g.graph[3])
+        self.assertIn([4,11,6],g.graph[1])
+        
+    def test_network02(self):
+        g = graph_from_file("input/network0.2.in")
+        self.assertEqual(g.nb_nodes, 10)
+        self.assertEqual(g.nb_edges, 4)
+        
 if __name__ == '__main__':
     unittest.main()
